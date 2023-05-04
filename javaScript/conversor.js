d3ecd2bf3e7173a56a7bf882d777820ee8be43c8
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-05-03 21:10:47 -0300

    Conversões do Fahrenheit

--- javaScript/conversor.js
+++ javaScript/conversor.js
@@ -2,11 +2,28 @@ function converteTemperaturas() {
 	var valorDigitado = document.getElementById("valor").value;
 	var conversoes = document.getElementById("conversoes");
 	var conversaoSelecionada = document.getElementsByTagName("option");
-	for (var i = 0; i < conversoes.length; i++) {
-		if (conversaoSelecionada[i].value == "fahrenheitParaCelsius") {
-			
-		} else if (conversaoSelecionada[i].value == "fahrenheitParakelvin") {
 
+	var valorConvertido;
+
+	for (var i = 0; i < conversoes.length; i++) {
+		if (conversaoSelecionada[i].selected) {
+			if (conversaoSelecionada[i].value == "fahrenheitParaCelsius") {
+				valorConvertido = (valorDigitado - 32) * 5 / 9;
+				console.log(valorConvertido);
+			} else if (conversaoSelecionada[i].value == "fahrenheitParakelvin") {
+				valorConvertido = (valorDigitado - 32) * 5 / 9 + 273.15;
+				console.log(valorConvertido);
+			} else if (conversaoSelecionada[i].value == "fahrenheitParakelvin") {
+
+			} else if (conversaoSelecionada[i].value == "celsiusParaFahrenheit") {
+
+			} else if (conversaoSelecionada[i].value == "celsiusParaKelvin") {
+
+			} else if (conversaoSelecionada[i].value == "kelvinParaFahrenheit") {
+
+			} else if (conversaoSelecionada[i].value == "kelvinParaCelsius") {
+
+			} 
 		}
 	}
 }

38d5b1c0e108d8ef6967775dbbe019bc51cc0937
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2023-05-04 11:40:43 -0300

    Centralização da imagem de fundo

--- javaScript/conversor.js
+++ javaScript/conversor.js
@@ -4,29 +4,35 @@ function converteTemperaturas() {
 	var conversaoSelecionada = document.getElementsByTagName("option");
 	var resposta = document.getElementById("resposta-conversao");
 
-	var valorConvertido;
+	if (valorDigitado.length == 0) {
+		alert("Digite o valor!");
+	} else if (isNaN(valorDigitado)) {
+		alert("Digite somente números!");
+	} else {
+		var valorConvertido;
 
-	for (var i = 0; i < conversoes.length; i++) {
-		if (conversaoSelecionada[i].selected) {
-			if (conversaoSelecionada[i].value == "fahrenheitParaCelsius") {
-				valorConvertido = (valorDigitado - 32) * 5 / 9;
-				resposta.innerHTML = valorConvertido.toFixed(2) + "°C";
-			} else if (conversaoSelecionada[i].value == "fahrenheitParakelvin") {
-				valorConvertido = (valorDigitado - 32) * 5 / 9 + 273.15;
-				resposta.innerHTML = valorConvertido.toFixed(2) + "K";
-			} else if (conversaoSelecionada[i].value == "celsiusParaFahrenheit") {
-				valorConvertido = valorDigitado * 9 / 5 + 32;
-				resposta.innerHTML = valorConvertido.toFixed(2) + "°F";
-			} else if (conversaoSelecionada[i].value == "celsiusParaKelvin") {
-				valorConvertido = valorDigitado + 273.15;
-				resposta.innerHTML = valorConvertido.toFixed(2) + "K";
-			} else if (conversaoSelecionada[i].value == "kelvinParaFahrenheit") {
-				valorConvertido = valorDigitado + 273.15;
-				resposta.innerHTML = valorConvertido.toFixed(2) + "°F";
-			} else if (conversaoSelecionada[i].value == "kelvinParaCelsius") {
-				valorConvertido = valorDigitado - 273.15;
-				resposta.innerHTML = valorConvertido.toFixed(2) + "°C";
-			} 
+		for (var i = 0; i < conversoes.length; i++) {
+			if (conversaoSelecionada[i].selected) {
+				if (conversaoSelecionada[i].value == "fahrenheitParaCelsius") {
+					valorConvertido = (valorDigitado - 32) * 5 / 9;
+					resposta.innerHTML = valorConvertido.toFixed(2) + "°C";
+				} else if (conversaoSelecionada[i].value == "fahrenheitParakelvin") {
+					valorConvertido = (valorDigitado - 32) * 5 / 9 + 273.15;
+					resposta.innerHTML = valorConvertido.toFixed(2) + "K";
+				} else if (conversaoSelecionada[i].value == "celsiusParaFahrenheit") {
+					valorConvertido = valorDigitado * 9 / 5 + 32;
+					resposta.innerHTML = valorConvertido.toFixed(2) + "°F";
+				} else if (conversaoSelecionada[i].value == "celsiusParaKelvin") {
+					valorConvertido = valorDigitado + 273.15;
+					resposta.innerHTML = valorConvertido.toFixed(2) + "K";
+				} else if (conversaoSelecionada[i].value == "kelvinParaFahrenheit") {
+					valorConvertido = valorDigitado + 273.15;
+					resposta.innerHTML = valorConvertido.toFixed(2) + "°F";
+				} else if (conversaoSelecionada[i].value == "kelvinParaCelsius") {
+					valorConvertido = valorDigitado - 273.15;
+					resposta.innerHTML = valorConvertido.toFixed(2) + "°C";
+				} 
+			}
 		}
 	}
 }
